0b7d80e6c2a129a75e3fbb5147ebddca7ca3bcda
Gagondeep Srai <gagondeep.srai@gmail.com>
2017-09-11 14:54:48 +0100

    added empty directories to git, added cleanup code to deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 rm -rf .build
 mkdir .build
 
+rm resources/public/js/main.min.js
 webpack -p
 
 cp -r resources/public/* .build/
+rm resources/public/js/main.min.js
 git checkout master
 cp -R .build/* .
 git add --all
