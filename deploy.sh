98c2eac3cc2f3472f3166beeff857908207ea03a
Gagondeep Srai <gagondeep.srai@gmail.com>
2018-11-11 14:23:23 +0000

    Update deploy script to remove old build artifacts in master branch

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@
 npm run build
 # copy build artifacts to master and commit to deploy 
 git checkout master
+rm *.js
+rm *.css
 cp -R dist/* .
 git add --all
 git commit -m "deployed release candidate"
