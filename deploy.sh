ab785f5323b7aa340bdfc9e0833c63a44ba38f98
Gagondeep Srai <gagondeep.srai@gmail.com>
2018-11-11 14:12:04 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# create a brand new .build directory
-rm -rf .build
-mkdir .build
 # clean and build
 npm run build
-# copy dist to build
-cp -r dist/* .build/
 # copy build artifacts to master and commit to deploy 
 git checkout master
-cp -R .build/* .
+cp -R dist/* .
 git add --all
 git commit -m "deployed release candidate"
 git push origin master
