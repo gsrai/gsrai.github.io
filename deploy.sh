a34ef326497c5755f26f06fe8f61cb31751a8d83
Gagondeep Srai <gagondeep.srai@gmail.com>
2018-11-13 20:50:13 +0000

    Update deploy script, using .build directory as dist/assets is now under vcs and cannot be copied over to master via the dist directory

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+rm -rf .build
+mkdir -p .build
+
 # clean and build
 npm run build
+
 # copy build artifacts to master and commit to deploy 
+cp -R dist/ .build
 git checkout master
 rm *.js
 rm *.css
-cp -R dist/* .
+cp -R .build/* .
 git add --all
 git commit -m "deployed release candidate"
 git push origin master
